dfb9ac489133fc3767a0c4b1b611b90317058179
danheeks <danheeks@gmail.com>
2018-08-03 09:19:20 +0000

    Trying to get CAD to build with python3 on RPI

--- CAD/App.cpp
+++ CAD/App.cpp
@@ -1971,43 +1971,13 @@ geoff_geometry::Matrix CApp::GetDrawMatrix(bool get_the_appropriate_orthogonal)
 
 extern void PythonOnMessageBox(const wchar_t* message);
 
-#ifndef WIN32
-int MessageBox(HWND hwnd, const char* text, const char* caption, UINT type)
-{
-	GtkWidget *dialog ;
-
-	/* Instead of 0, use GTK_DIALOG_MODAL to get a modal dialog box */
-
-	if (type & MB_YESNO)
-		dialog = gtk_message_dialog_new(NULL, 0, GTK_MESSAGE_QUESTION, GTK_BUTTONS_YES_NO, text );
-	else
-		dialog = gtk_message_dialog_new(NULL, 0, GTK_MESSAGE_INFO, GTK_BUTTONS_OK, text );
-
-
-	gtk_window_set_title(GTK_WINDOW(dialog), caption);
-	gint result = gtk_dialog_run(GTK_DIALOG(dialog));
-	gtk_widget_destroy( GTK_WIDGET(dialog) );
-
-	if (type & MB_YESNO)
-	{
-		switch (result)
-		{
-		default:
-		case GTK_RESPONSE_DELETE_EVENT:
-		case GTK_RESPONSE_NO:
-			return IDNO;
-		case GTK_RESPONSE_YES:
-			return IDYES;
-		}
-		return IDOK;
-	}
-}
-#endif
-
-
 void CApp::MessageBox(const wchar_t* message)
 {
+#ifdef WIN32
 	::MessageBox(NULL, message, L"Message", MB_OK);
+#else
+	PythonOnMessageBox(message);
+#endif
 }
 
 void CApp::GetOptions(std::list<Property *> *list)
