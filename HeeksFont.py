f87c6e90f4f45035c9db96d943ca562bf5a28236
danheeks <danheeks@gmail.com>
2020-05-17 16:49:38 +0100

    I finished adding arial font.

--- HeeksFont.py
+++ HeeksFont.py
@@ -65,10 +65,8 @@ def MakeCurveFromD(d, group_name):
                     else: curp.y -= float(nums)
                     curve.Append(curp)
                 elif mode == 'cy1':
-                    print('cy1')
                     mode = 'cx2'
                 elif mode == 'cy2':
-                    print('cy2')
                     mode = 'x'
                 nums = ''
         elif c == ',':
@@ -77,10 +75,8 @@ def MakeCurveFromD(d, group_name):
                 else: curp.x += float(nums)
                 mode = 'y'
             elif mode == 'cx1':
-                print('cx1')
                 mode = 'cy1'
             elif mode == 'cx2':
-                print('cx2')
                 mode = 'cy2'
             else:
                 raise NameError("mode " + mode + " not expecting comma!")
@@ -132,45 +128,43 @@ def ConvertHeeksFont():
                     expected_label = '{http://www.inkscape.org/namespaces/inkscape}label'
                     if expected_label in item.attrib:
                         label = item.attrib[expected_label]
-                    else:
-                        print('warning: ' + expected_label + ' not in g, item = ' + str(item) + "\n item.attrib['id'] = " + str(item.attrib['id']))
-                    if label[:4] == 'char' and len(label) == 5:
-                        c = label[4]
-                        curves = []
-                        tris = []
-                        pos = geom.Point(0,0)
-                        width = 10.0
-                        for path in item:
-                            stroke = None
-                            fill = None
-                            styles = path.attrib['style'].split(';')
-                            for style in styles:
-                                pair = style.split(':')
-                                if len(pair) == 2:
-                                    if pair[0] == 'stroke':
-                                        if len(pair[1]) == 7:
-                                            stroke = pair[1][1:7]
-                                        else:
-                                            stroke = None
-                                    elif pair[0] == 'fill':
-                                        if len(pair[1]) == 7:
-                                            fill = pair[1][1:7]
-                                        else:
-                                            fill = None
-                            
-                            curve = MakeCurveFromD(path.attrib['d'], c)
-                            box = curve.GetBox()
-                            if stroke == POSITION_COLOUR:
-                                pos = geom.Point(box.MinX(), box.MinY())
-                                width = box.MaxX() - box.MinX()
-                            elif stroke != None:
-                                curves.append(curve)
-                            elif fill != None:
-                                if curve.NumVertices() == 4:
-                                    # triangle curve
-                                    vertices = curve.GetVertices()
-                                    tris.append(Tri(vertices[0].p, vertices[1].p, vertices[2].p))
-                        chars[c] = Character(curves, pos, width, tris)
+                        if label[:4] == 'char' and len(label) == 5:
+                            c = label[4]
+                            curves = []
+                            tris = []
+                            pos = geom.Point(0,0)
+                            width = 10.0
+                            for path in item:
+                                stroke = None
+                                fill = None
+                                styles = path.attrib['style'].split(';')
+                                for style in styles:
+                                    pair = style.split(':')
+                                    if len(pair) == 2:
+                                        if pair[0] == 'stroke':
+                                            if len(pair[1]) == 7:
+                                                stroke = pair[1][1:7]
+                                            else:
+                                                stroke = None
+                                        elif pair[0] == 'fill':
+                                            if len(pair[1]) == 7:
+                                                fill = pair[1][1:7]
+                                            else:
+                                                fill = None
+                                
+                                curve = MakeCurveFromD(path.attrib['d'], c)
+                                box = curve.GetBox()
+                                if stroke == POSITION_COLOUR:
+                                    pos = geom.Point(box.MinX(), box.MinY())
+                                    width = box.MaxX() - box.MinX()
+                                elif stroke != None:
+                                    curves.append(curve)
+                                elif fill != None:
+                                    if curve.NumVertices() == 4:
+                                        # triangle curve
+                                        vertices = curve.GetVertices()
+                                        tris.append(Tri(vertices[0].p, vertices[1].p, vertices[2].p))
+                            chars[c] = Character(curves, pos, width, tris)
     
     # now write the c++ file
     f = open(this_dir + '/CAD/HeeksFontData.h', 'w')
@@ -264,30 +258,39 @@ def ConvertHeeksFont():
         for v in vertices:
             points.append(v.p)
             
-    f.write('static int num_points[' + str(len(all_curves)) + '] =   {')
+    if len(num_points) == 0:
+        f.write('static int* num_points;\n')
+    else:
+        f.write('static int num_points[' + str(len(num_points)) + '] =   {')
 
-    i = 0
-    for n in num_points:
-        f.write(str(n) + ', //' + str(i) + '\n')
-        i += 1
+        i = 0
+        for n in num_points:
+            f.write(str(n) + ', //' + str(i) + '\n')
+            i += 1
+            
+        f.write('};\n')
         
-    f.write('};\n')
-    
-    f.write('static int start_points[' + str(len(all_curves)) + '] = {')
+    if len(start_points) == 0:
+        f.write('static int* start_points;\n')
+    else:
+        f.write('static int start_points[' + str(len(start_points)) + '] = {')
 
-    i = 0
-    for n in start_points:
-        f.write(str(n) + ', //' + str(i) + '\n')
-        i += 1
-        
-    f.write('};\n')
+        i = 0
+        for n in start_points:
+            f.write(str(n) + ', //' + str(i) + '\n')
+            i += 1
+            
+        f.write('};\n')
     
-    i = 0
-    f.write('static float points[' + str(len(points)) + '][2] = {\n')
-    for p in points:
-        f.write('       {' + str(float(p.x)) + 'f, ' + str(float(p.y)) + 'f}, //' + str(i) + '\n')
-        i += 1
-    f.write('};\n')
+    if len(points) == 0:
+        f.write('static float** points;\n')
+    else:
+        i = 0
+        f.write('static float points[' + str(len(points)) + '][2] = {\n')
+        for p in points:
+            f.write('       {' + str(float(p.x)) + 'f, ' + str(float(p.y)) + 'f}, //' + str(i) + '\n')
+            i += 1
+        f.write('};\n')
             
     f.write('static int num_triangles[95] =   {')
 
